3fa7b0a3c59017be0bd429aa79467eebe36ea564
MaxKicw <maximilian.wick@student.hs-rm.de>
2018-05-15 11:57:17 +0200

    Revert "BugFix Proximity"
    This reverts commit 7a03671d9fa499072a0d2f4c19d99e31612e1cde.

--- www/js/index.js
+++ www/js/index.js
@@ -119,8 +119,8 @@ function proximitySuccess(proximity){
 	document.getElementById('5').innerHTML = '';
 	var node = document.createElement('div');
 	if(proximity === true){
-		node.innerHTML = "<p>Der Abstandsensor ist bedeckt!";
+		node.innerHTML = "<p>Der Abstandsensor ist bedeckt!"
 	}else{
-		node.innerHTML = "<p>Der Abstandssensor ist nicht bedeckt!";
+		node.innerHTML = "<p>Der Abstandssensor ist nicht bedeckt!"
 	}
 }
