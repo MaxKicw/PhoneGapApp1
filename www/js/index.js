6fce5b9fb1596a749c967fe0bde89455e04ab013
MaxKicw <m142679@googlemail.com>
2018-12-12 13:54:56 +0100

    Debugging stuff added

--- www/js/index.js
+++ www/js/index.js
@@ -52,14 +52,16 @@ var app = {
 		window.plugins.PushbotsPlugin.on("notification:received", function(data){
 			if(app.track){
 				app.trackedActivity = currentAcitvity;
+				alert(JSON.stringify(app.trackedActivity));
 				app.user = device.uuid;
-				const date = moment().format("DD-MM-YY ");
+				const date = moment().format("DD.MM.YY ");
 				const time = moment().format("HH:mm");
 				app.timestamp_push = moment().format("DD MM YY HH mm ss");
 				document.getElementById('q1').classList.add('active');
 				document.getElementById('intro').classList.remove('active');
 				document.getElementById('frage').innerText = 'Wir haben dir um '+time+' am '+date+' Uhr eine Push-Nachricht zugestellt! Laut unserer Acitvity-Tracking-App hast Du zu diesem Zeitpunkt folgendes gemacht: ';
 				let hightestValue = Object.keys(app.trackedActivity).reduce(function(a, b){ return obj[a] > obj[b] ? a : b });
+				alert(hightestValue);
 				let activityMessage;
 				switch(hightestValue){
 					case "STILL":
