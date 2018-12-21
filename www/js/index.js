5cd34f787ff28729ee71562e82e3af752f759482
MaxKicw <m142679@googlemail.com>
2018-12-21 21:26:26 +0100

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -33,10 +33,10 @@ var app = {
 	// function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
 		//For JSON Can
-		document.getElementById('track').innerText = 'Klicke damit keine Daten gesendet werden!';
-		document.getElementById('track-btn').style.backgroundColor = "#46A364";
-		app.receivedEvent('deviceready');
-		document.getElementById('rdy-btn').style.backgroundColor = "#46A364";
+		// document.getElementById('track').innerText = 'Klicke damit keine Daten gesendet werden!';
+		// document.getElementById('track-btn').style.backgroundColor = "#46A364";
+		// app.receivedEvent('deviceready');
+		// document.getElementById('rdy-btn').style.backgroundColor = "#46A364";
 	
 		// Only with First time registration - For Pushbot   878e3aaca8af23571d71081f8c0374b5
 			//Diese Funktion wird ausgeführt, wenn die App eine Nachricht erhalten hat
@@ -130,19 +130,19 @@ var app = {
 
         console.log('Received Event: ' + id);
 	},
-	push: function(){
-		window.plugins.PushbotsPlugin.initialize("5b151b591db2dc70b473dcb0", {"android":{"sender_id":"687741121085"}});
-		window.plugins.PushbotsPlugin.on("registered", 		function(token){
-			alert("Registration Id:" + token);
-		});
+	// push: function(){
+	// 	window.plugins.PushbotsPlugin.initialize("5b151b591db2dc70b473dcb0", {"android":{"sender_id":"687741121085"}});
+	// 	window.plugins.PushbotsPlugin.on("registered", 		function(token){
+	// 		alert("Registration Id:" + token);
+	// 	});
 	
-			//Get user registrationId/token and userId on PushBots, with evey launch of the app even launching with notification
+	// 		//Get user registrationId/token and userId on PushBots, with evey launch of the app even launching with notification
 			
-		window.plugins.PushbotsPlugin.on("user:ids", 	function(data){
-			console.log("user:ids" + JSON.stringify(data));
-		});
-		alert("NOTI")
-	}
+	// 	window.plugins.PushbotsPlugin.on("user:ids", 	function(data){
+	// 		console.log("user:ids" + JSON.stringify(data));
+	// 	});
+	// 	alert("NOTI")
+	// }
 };
 var serverURL = 'http://caebus.de/hackathon/testapp/testapp.php';//ServerURL
 //---------------Define antwort vars ----------------//
